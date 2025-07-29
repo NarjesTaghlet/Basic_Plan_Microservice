71e3c3b4b5a5257af6dcb1386691d9dfefe096f7
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-07-29 11:17:09 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    //const app = await NestFactory.create(AppModule);
    app.enableCors({
-     origin: ['http://localhost:4200','https://d399hhf7pq6yc6.cloudfront.net'] ,// ✅ Autoriser uniquement le frontend Angular
+     origin: ['http://localhost:4200','https://d1fsr28fh19wdy.cloudfront.net'] ,// ✅ Autoriser uniquement le frontend Angular
      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
      allowedHeaders: 'Content-Type,Authorization',
      credentials: true, // ✅ Si besoin d'authentification (JWT, Cookies)
@@ -17,7 +17,7 @@ async function bootstrap() {
 
     const corsMiddleware = (req, res, next) => {
       const allowedOrigins = [
-      'https://d399hhf7pq6yc6.cloudfront.net'
+      'https://d1fsr28fh19wdy.cloudfront.net'
       ];
       
       const origin = req.headers.origin;
